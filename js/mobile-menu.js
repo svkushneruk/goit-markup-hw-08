00929ae4472632fd142137d52e425e4b6e71fd7a
Serhii Kushneruk <svkushneruk@ukr.net>
2022-02-08 22:42:47 +0200

    Вносить правки: webp, aria-label...

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openMenuBtn.addEventListener('click', toggleMenu);
-//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-//   function toggleMenu() {
-//     refs.menu.classList.toggle('is-hidden');
-//   }
-// })();
-
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
@@ -35,7 +20,7 @@
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 768px)').addEventListener('resize', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
